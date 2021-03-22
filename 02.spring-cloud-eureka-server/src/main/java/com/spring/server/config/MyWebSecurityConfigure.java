76adfcc41b0bda9f3abb1404138afcbd78e9f792
mayitbe65 <yu362172432@gmail.com>
2021-03-22 20:15:12 +0800

    1.添加security安全校验

--- 02.spring-cloud-eureka-server/src/main/java/com/spring/server/config/MyWebSecurityConfigure.java
+++ 02.spring-cloud-eureka-server/src/main/java/com/spring/server/config/MyWebSecurityConfigure.java
@@ -1,16 +1,20 @@
 package com.spring.server.config;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * @author yu.zhang
  * created on 2021/03/17
  */
-//@EnableWebSecurity
-//public class MyWebSecurityConfigure extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-////        http.csrf().ignoringAntMatchers("/eureka/**");
-//        super.configure(http);
-//    }
-//}
+@EnableWebSecurity
+public class MyWebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+//        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
