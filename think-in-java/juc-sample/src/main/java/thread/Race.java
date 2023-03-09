ce23286fbaf98cc1749bf18b083ee0b3d42b3026
myitbe65 <yu362172432@gmail.com>
2023-03-10 01:41:35 +0800

    龟兔赛跑多线程

---
+++ think-in-java/juc-sample/src/main/java/thread/Race.java
@@ -0,0 +1,36 @@
+package thread;
+
+public class Race implements Runnable{
+
+    private static String winner;
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(Thread.currentThread().getName() + "跑了第" + i + "步");
+            if (gameOver(i)) {
+                break;
+            }
+        }
+    }
+
+    public boolean gameOver(Integer step) {
+        if (winner != null) {
+            return true;
+        }
+
+        if (step >= 100) {
+            winner = Thread.currentThread().getName();
+            System.out.println("冠军是: " + winner);
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Race race = new Race();
+        new Thread(race, "兔子").start();
+        new Thread(race, "乌龟").start();
+    }
+}
